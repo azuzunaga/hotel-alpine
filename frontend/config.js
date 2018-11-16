def4761428a5b3dc434342b474483a76c7a6089f
Americo <ame.zuz@gmail.com>
2018-11-15 18:46:50 -0800

    Create env variable for cloudinary API

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,4 @@
-// This is client side config only
+// This is client side config only, no sensitive data!
 export const endpoint = `http://localhost:5555`;
 export const perPage = 4;
+export const cloudinaryAPI = 'https://api.cloudinary.com/v1_1/lucha/image/upload';
