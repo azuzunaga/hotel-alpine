c9060e0d5b54c6e4688c5fca61030ad8de010f30
Americo <ame.zuz@gmail.com>
2018-11-15 18:47:26 -0800

    Create a global add button

---
+++ frontend/components/Add.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const AddButton = styled.img`
+  width: 7rem;
+  height: 7rem;
+  position: absolute;
+  bottom: 5vmin;
+  right: 5vmin;
+`;
+
+const Add = props => (
+  <Link href="/add">
+    <a><AddButton src="/../static/plus.svg"/></a>
+  </Link>
+);
+
+export default Add;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,10 @@
-const Index = () => (
+import Add from '../components/Add';
+
+const Home = () => (
   <div>
     <p>Welcome to Hotel Alpine</p>
+    <Add />
   </div>
 );
 
-export default Index;
+export default Home;
