937981ba417ed65781e4d4e63cb84f877d0ed0a3
Americo <ame.zuz@gmail.com>
2018-11-16 21:02:50 -0800

    Create single employee component

---
+++ frontend/components/Employee.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Employee extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+
+export default Employee;
