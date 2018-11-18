da8a3875dd9827e5a17f3e2ba08e2fa04e75ef05
Americo <ame.zuz@gmail.com>
2018-11-18 12:24:29 -0800

    Simplify mutations
    Once there is need to intercept the request this can be modified,
    but for now it is ok for yoga to forward the request to Prisma directly.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,19 +1,8 @@
+const { forwardTo } = require('prisma-binding');
+
 const Mutations = {
-  async createUser(parent, args, ctx, info) {
-    const user = await ctx.db.mutation.createUser(
-      {
-        data: {
-          ...args,
-        },
-      },
-      info,
-    );
-    return user;
-  },
-  async deleteUser(parent, args, ctx, info) {
-    const where = { id: args.id };
-    return ctx.db.mutation.deleteUser({ where }, info);
-  },
+  createUser: forwardTo('db'),
+  deleteUser: forwardTo('db'),
 };
 
 module.exports = Mutations;
