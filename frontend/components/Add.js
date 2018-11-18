94bc2ba880f95b283162a06ac8405e3bb91d7499
Americo <ame.zuz@gmail.com>
2018-11-17 16:27:27 -0800

    Use text instead of an SVG

--- frontend/components/Add.js
+++ frontend/components/Add.js
@@ -1,17 +1,24 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 
-const AddButton = styled.img`
-  width: 7rem;
-  height: 7rem;
+const AddButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 5rem;
+  height: 5rem;
   position: fixed;
   bottom: 5vmin;
   right: 5vmin;
+  font-size: 7rem;
+  &:hover {
+    color: ${props => props.theme.blue};
+  }
 `;
 
 const Add = ({ addPath }) => (
   <Link href={`/new${addPath}`}>
-    <a><AddButton src="/../static/plus.svg"/></a>
+    <a><AddButton >+</ AddButton></a>
   </Link>
 );
 
