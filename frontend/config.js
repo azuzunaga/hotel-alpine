0b173c4dab1fd86fbeda5c138e117ebf9edc0851
Americo <ame.zuz@gmail.com>
2018-11-17 16:25:49 -0800

    Add rand user api

--- frontend/config.js
+++ frontend/config.js
@@ -2,4 +2,4 @@
 export const endpoint = `http://localhost:5555`;
 export const perPage = 4;
 export const CdnApiUrl = 'https://api.cloudinary.com/v1_1/lucha/image/upload';
-export const randUserApi = '';
+export const randUserApi = 'https://randomuser.me/api/?inc=name,picture&noinfo?nat=us';
