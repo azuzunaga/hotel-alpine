56c5311ec3a5418c13e0a0b31c018bdfd6f914ce
Americo <ame.zuz@gmail.com>
2018-11-19 13:17:31 -0800

    Handle mutation manually

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -3,7 +3,32 @@ const { forwardTo } = require('prisma-binding');
 const Mutations = {
   createUser: forwardTo('db'),
   deleteUser: forwardTo('db'),
-  updateUser: forwardTo('db'),
+  async updateUser(parent, args, ctx, info) {
+    const { where, data } = args;
+    const { department, location } = data;
+    /*
+    Check to see if department or location came with data, delete
+    if not. This is necessary because both fields reference other
+    tables, so they come with a nested object that references the
+    id, or just a nested object.
+    */
+    const depId = department.connect.id;
+    const locId = location.connect.id;
+    if (!depId) {
+      delete data.department;
+    }
+    if (!locId) {
+      delete data.location;
+    }
+
+    return ctx.db.mutation.updateUser(
+      {
+        data,
+        where,
+      },
+      info,
+    );
+  },
 };
 
 module.exports = Mutations;
