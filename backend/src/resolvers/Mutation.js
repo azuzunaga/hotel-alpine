7b7f1ddc80be97ad5e906cc4ce9c8c63c8173e45
Americo <ame.zuz@gmail.com>
2018-11-19 17:04:41 -0800

    Add createDepartment workflow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -12,19 +12,37 @@ const Mutations = {
     tables, so they come with a nested object that references the
     id, or just a nested object.
     */
-    const depId = department.connect.id;
-    const locId = location.connect.id;
+    const { id: depId } = department.connect;
+    const { id: locId } = location.connect;
     if (!depId) {
       delete data.department;
     }
     if (!locId) {
       delete data.location;
     }
-
     return ctx.db.mutation.updateUser(
       {
         data,
         where,
+      },
+      info,
+    );
+  },
+  async createDepartment(parent, args, ctx, info) {
+    const { data } = args;
+    const departments = await ctx.db.query.departments(
+      {
+        where: {
+          name_contains: data.name,
+        },
+      },
+    );
+    if (departments.length !== 0) {
+      throw new Error('A department with that name already exists');
+    }
+    return ctx.db.mutation.createDepartment(
+      {
+        data,
       },
       info,
     );
