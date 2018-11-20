ff11cdb171844d2eb5d3153744df7d197cefaa28
Americo <ame.zuz@gmail.com>
2018-11-19 19:16:38 -0800

    Add search funtionality

--- frontend/components/Employees.js
+++ frontend/components/Employees.js
@@ -5,8 +5,18 @@ import gql from 'graphql-tag';
 import Employee, { UserEntry } from './Employee';
 
 export const ALL_EMPLOYEES_QUERY = gql`
-  query ALL_EMPLOYEES_QUERY {
-    users(orderBy: name_ASC) {
+  query ALL_EMPLOYEES_QUERY($search: String) {
+    users (orderBy: name_ASC, where: {
+      OR: [
+        {name_contains: $search},
+        {department: {
+          name_contains: $search
+        }},
+        {location: {
+          city_contains: $search
+        }}
+      ]
+    }) {
       id
       name
       department {
@@ -29,17 +39,73 @@ const UsersList = styled.div`
   max-width: ${props => props.theme.maxWidth};
 `;
 
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  justify-items: center;
+  label {
+    width: 100%;
+  }
+  input {
+    width: 100%;
+    font-size: 1.6rem;
+    padding: 5px;
+    border-radius: 5px;
+  }
+`;
+
+const Spinner = styled.div`
+  &.true {
+    width: 18px;
+    height: 18px;
+    box-sizing: border-box;
+    border: solid 2px transparent;
+    border-top-color: ${props => props.theme.blue};
+    border-left-color: ${props => props.theme.blue};
+    border-radius: 50%;
+    position: relative;
+    left: -25px;
+  
+    -webkit-animation: nprogress-spinner 400ms linear infinite;
+            animation: nprogress-spinner 400ms linear infinite;
+  }
+  width: 18px;
+`;
+
 
 class Employees extends Component {
+  state = {
+    search: '',
+  }
+
+  handleChange = (e, loading) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
       <Center>
-        <Query query={ALL_EMPLOYEES_QUERY}>
+        <Query query={ALL_EMPLOYEES_QUERY} variables={this.state}>
           {({ data, loading, error }) => {
-            if (loading) return <p>Loading...</p>;
             if (error) return <p>Error</p>;
             return (
               <UsersList>
+                <Search>
+                  <label htmlFor="search">
+                    <input
+                      type="text"
+                      name="search"
+                      id="search"
+                      placeholder="Search all the things!"
+                      value={this.state.search}
+                      onChange={e => {
+                        this.handleChange(e, loading);
+                      }}
+                    />
+                  </label>
+                  <Spinner id="spinner" className={loading} />
+                </Search>
                 <UserEntry>
                   <span></span>
                   <h4>Name</h4>
